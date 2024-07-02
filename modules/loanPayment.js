0ac91440bc2f3d66d99597d4fe3d5f8d89af5bf5
Tristen Roupp <tristenr@usf.edu>
2024-07-02 17:06:33 -0400

    Create loan payment calculation module

--- modules/loanPayment.js
+++ modules/loanPayment.js
@@ -1,4 +1,4 @@
-
+// modules/loanPayment.js
 export function calculateLoanPayment(principal, rate, n) {
     const monthlyRate = rate / 12;
     return (principal * monthlyRate * Math.pow(1 + monthlyRate, n)) / (Math.pow(1 + monthlyRate, n) - 1);
